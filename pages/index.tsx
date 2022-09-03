8c39b95a6eda24ac087164a99fa1be30e928da48
Vincent Liao <vincent.liao@protonmail.com>
2022-09-03 10:10:27 +0700

    store privkey and delegate event inside localStorage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 // import Head from "next/head";
 // import Image from "next/image";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useSignMessage } from "wagmi";
+import { useSignMessage, useAccount } from "wagmi";
 import { useState, useEffect } from "react";
 import * as secp from "@noble/secp256k1";
 
@@ -17,24 +17,58 @@ interface EventInterface {
 }
 
 export default function Home() {
+  // idea: also store delegation event to localStorage
+  //   let pubkeyHex: string;
+  // let nostrPrivkey: string;
   const [nostrPrivkey, setNostrPrivkey] = useState("");
+  const [pubkeyHex, setPubkeyHex] = useState("");
 
+  let ethAddress: string | undefined;
+  const { address } = useAccount();
+  ethAddress = address;
+
+  const delegateMessage = JSON.stringify({
+    authority: `${ethAddress}`,
+    schnorrDelegate: `${pubkeyHex}`,
+  });
+
+  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+    message: delegateMessage,
+  });
+
+  // on render
   useEffect(() => {
     const storagePrivkey = localStorage.getItem("nostrPrivkey");
     if (!storagePrivkey) {
       const privkey = secp.utils.randomPrivateKey();
       const privkeyHex = secp.utils.bytesToHex(privkey);
-      console.log(privkeyHex);
-      localStorage.setItem("nostrPrivkey", privkeyHex);
-      setNostrPrivkey(nostrPrivkey);
+      setNostrPrivkey(privkeyHex);
+      const pubkey = secp.schnorr.getPublicKey(privkeyHex);
+      setPubkeyHex(secp.utils.bytesToHex(pubkey));
     } else {
       setNostrPrivkey(storagePrivkey);
+      const pubkey = secp.schnorr.getPublicKey(storagePrivkey);
+      setPubkeyHex(secp.utils.bytesToHex(pubkey));
     }
   }, []);
 
-  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
-    message: "gm wagmi frens",
-  });
+  // on sig success
+  useEffect(() => {
+    if (isSuccess) {
+      const delegateJSON = JSON.parse(delegateMessage);
+      const delegateWithSig = { delegate: delegateJSON, sig: data };
+
+      console.log(JSON.stringify(delegateWithSig));
+      console.log(nostrPrivkey);
+      localStorage.setItem("nostrPrivkey", nostrPrivkey);
+      localStorage.setItem(
+        "nostrDelegateEvent",
+        JSON.stringify(delegateWithSig)
+      );
+      // store event and privkey to localStorage
+      // stringify this, wrap it in NIP-01
+    }
+  }, [isSuccess]);
 
   return (
     <div>
@@ -44,8 +78,8 @@ export default function Home() {
       </button>
       {isSuccess && <div>Signature: {data}</div>}
       {isError && <div>Error signing message</div>}
-      <button onClick={() => setNostrPrivkey("ofjwoefjfff")}>setschnorr</button>
-      <p>{nostrPrivkey}</p>
+      <p>{`priv: ${nostrPrivkey}`}</p>
+      <p>{`pub: ${pubkeyHex}`}</p>
     </div>
   );
 }
